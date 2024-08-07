2f239a30cd978950d8a769b3c9e66811b9d5937b
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2024-08-08 01:17:32 +0530

    Initial

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Homepage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import Content from "./Content";
+import Hero from "./Hero";
+
+function Homepage(){
+    return(
+        <>
+        <Hero/>
+        <Content/>
+        </>
+    )
+}
+
+export default Homepage;
